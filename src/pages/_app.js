76544a3739e9f404db6e0e2f6ec3dda99a68f5aa
thatanjan <anjan.cules@gmail.com>
2021-06-14 17:22:31 +0600

    material ui for server side rendering  setup is done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+
 function MyApp({ Component, pageProps }) {
+	useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector('#jss-server-side')
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles)
+		}
+	}, [])
+
 	return <Component {...pageProps} />
 }
 
