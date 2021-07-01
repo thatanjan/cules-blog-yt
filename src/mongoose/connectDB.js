6c78fa60f875e166602585682a53e51f34f924ed
thatanjan <anjan.cules@gmail.com>
2021-07-01 16:05:43 +0600

    connectDb and blog model is created

---
+++ src/mongoose/connectDB.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const { MONGO_URI } = process.env
+
+if (!MONGO_URI) {
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local'
+	)
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+	cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+	if (cached.conn) {
+		return cached.conn
+	}
+
+	if (!cached.promise) {
+		const opts = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			bufferCommands: false,
+			bufferMaxEntries: 0,
+			useFindAndModify: false,
+			useCreateIndex: true,
+		}
+
+		cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+			return mongoose
+		})
+	}
+	cached.conn = await cached.promise
+	return cached.conn
+}
+
+export default dbConnect
