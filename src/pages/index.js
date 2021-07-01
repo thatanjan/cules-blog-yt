4a3e2087a382cab604ac52bf1cd1872f49b5597c
thatanjan <anjan.cules@gmail.com>
2021-07-01 16:05:09 +0600

    mdx files parsed and uploaded to database

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
+import readingTime from 'reading-time'
+import matter from 'gray-matter'
+import fs from 'fs'
+import path from 'path'
+
+import connectDB from '../mongoose/connectDB'
+import BlogModel from '../mongoose/BlogModel'
+
 import BlogPreviewLayout from '../components/Layouts/BlogPreviewLayout'
 
 export default function Home() {
@@ -6,4 +14,54 @@ export default function Home() {
 			<BlogPreviewLayout />
 		</>
 	)
+}
+
+export const getStaticProps = async () => {
+	await connectDB()
+
+	const root = process.cwd()
+
+	const allBlogFileNames = fs.readdirSync(path.join(root, 'src', 'blogs'))
+
+	const fileDatas = allBlogFileNames.map((fileName) => {
+		const fileData = fs.readFileSync(
+			path.join(root, 'src', 'blogs', fileName),
+			'utf8'
+		)
+
+		const matterData = matter(fileData)
+
+		const slug = fileName.replace('.mdx', '')
+
+		const { data } = matterData
+
+		data.slug = slug
+
+		return matterData
+	})
+
+	const updateBlogPromise = fileDatas.map((fileData) => {
+		const { data, content } = fileData
+
+		return BlogModel.updateOne(
+			{ customID: data.customID },
+			{
+				$set: {
+					...data,
+					content,
+					readingTime: readingTime(content).text,
+				},
+			},
+			{
+				upsert: true,
+				setDefaultsOnInsert: true,
+			}
+		)
+	})
+
+	await Promise.all(updateBlogPromise)
+
+	return {
+		props: {},
+	}
 }
