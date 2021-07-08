09e52b84e2f55509b934905970dfc1c503fea96d
thatanjan <anjan.cules@gmail.com>
2021-07-08 17:37:12 +0600

    search feature added to the dynamic page

--- src/pages/search/[query].jsx
+++ src/pages/search/[query].jsx
@@ -3,16 +3,49 @@ import Typography from '@material-ui/core/Typography'
 
 import BlogPreviewLayout from '../../components/Layouts/BlogPreviewLayout'
 
-const Query = () => {
+import BlogModel from '../../mongoose/BlogModel'
+import connectDB from '../../mongoose/connectDB'
+
+const Query = ({ allBlogs }) => {
 	return (
 		<>
 			<Typography color='secondary' variant='h2' component='h1'>
 				You have X search results based on the Query
 			</Typography>
 
-			<BlogPreviewLayout />
+			<BlogPreviewLayout allBlogs={allBlogs} />
 		</>
 	)
+}
+
+export const getServerSideProps = async ({ query: { query } }) => {
+	await connectDB()
+
+	const queryString = query.replace(/\+/g, ' ').trim()
+
+	const project = {
+		_id: 0,
+		__v: 0,
+		content: 0,
+	}
+
+	const result = await BlogModel.find(
+		{
+			$text: {
+				$search: queryString,
+			},
+		},
+		project
+	).sort({ score: { $meta: 'textScore' } })
+
+	const allBlogs = result.map((blog) => {
+		blog = blog.toObject()
+		blog.createdAt = blog.createdAt.toDateString()
+
+		return blog
+	})
+
+	return { props: { allBlogs } }
 }
 
 export default Query
